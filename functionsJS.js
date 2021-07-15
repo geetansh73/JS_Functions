c8c186d6c6ebda906f00a014cbe47dc77bc835f9
Geetansh Gupta <67698473+geetansh73@users.noreply.github.com>
2021-07-15 11:28:35 +0530

    Create functionsJS.js

---
+++ functionsJS.js
@@ -0,0 +1,41 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+/*
+
+ */
+function factorial(n)
+{
+    var f=1;
+for(var i=n;i>=1;i--)
+{
+f=f*i;
+}
+return f;
+}
+
+function main() {
+    const n = +(readLine());
+    
+    console.log(factorial(n));
+}
